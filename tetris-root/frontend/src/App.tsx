8a1c40e8c4e869c4f9ade1c246dae9aba39128a2
rachelish <racheltodd50@gmail.com>
2025-10-09 18:46:41 -0400

    new stuff

--- tetris-root/frontend/src/App.tsx
+++ tetris-root/frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>MEOW awawawa</h1>
+      <h1>jfgjfgfh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
