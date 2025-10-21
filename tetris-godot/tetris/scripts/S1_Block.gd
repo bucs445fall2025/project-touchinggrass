4cf04cd73e571905f7a71e74bd1a965d7fd5ab6f
rachelish <racheltodd50@gmail.com>
2025-10-21 03:15:00 -0400

    collisions added but boundaries are off

--- tetris-godot/tetris/scripts/S1_Block.gd
+++ tetris-godot/tetris/scripts/S1_Block.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var move_speed: float = 20.0
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_down: RayCast2D = $RayCastDown
+@onready var ray_cast_up: RayCast2D = $RayCastUp
 
 var target_pos: Vector2
 var moving := false
@@ -19,10 +21,19 @@ func _process(delta: float) -> void:
 			if !ray_cast_right.is_colliding():
 				target_pos.x += step_size; 
 				moving = true
+			else:
+				print("IS COLLIDING RIGHT")
 		elif Input.is_action_just_pressed("move_left"):
-			target_pos.x -= step_size; moving = true
+			if !ray_cast_left.is_colliding():
+				target_pos.x -= step_size; 
+				moving = true
+			else:
+				print("IS COLLIDING LEFT")
 		elif Input.is_action_just_pressed("move_down"):
-			target_pos.y += step_size; moving = true
+			if !ray_cast_down.is_colliding():
+				target_pos.y += step_size; moving = true
+			else:
+				print("IS COLLIDING DOWN")
 		#elif Input.is_action_just_pressed("rotate"):
 			#target_pos.angle() *= PI;
 			#moving = true
@@ -32,18 +43,3 @@ func _process(delta: float) -> void:
 		if position.distance_to(target_pos) < 0.5:
 			position = target_pos
 			moving = false
-
-
-
-
-#extends Node2D
-#
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
